c146e7c272b018d9d36bda680113e7326aa8b6f9
PetejStone <petejstoney@gmail.com>
2018-04-05 16:21:28 -0700

    created page transitions

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
 const navIcon = document.getElementById("nav-icon");
 const navMenu = document.getElementsByTagName('nav')[0];
+const mainContent = document.getElementsByTagName('main')[0].children;
+const closeButton = document.getElementsByClassName('close')
 navIcon.addEventListener('click',()=> {
   navIcon.classList.toggle("change");
   navMenu.classList.toggle("changeNav");
   navMenu.previousElementSibling.classList.toggle("changeSubheading");
   navMenu.previousElementSibling.previousElementSibling.classList.toggle("changeHeading");
-  console.log('change');
+
+  for (let i=0; i < mainContent.length; i++) {
+    mainContent[i].classList.toggle('show');
+  }
+
+  for (let i=0; i < navMenu.children.length; i++) {
+    navMenu.children[i].addEventListener('click', ()=> {
+      mainContent[i].classList.toggle('slideIn');
+    });
+  }
+
+  for (let i=0; i < closeButton.length; i++) {
+    closeButton[i].addEventListener('click', ()=> {
+      mainContent[i].classList.toggle('slideIn');
+    });
+  }
 });

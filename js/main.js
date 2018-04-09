6862c873dada26432f7d8a41cc0d9be455868513
PetejStone <petejstoney@gmail.com>
2018-04-09 14:13:24 -0700

    changed background

--- js/main.js
+++ js/main.js
@@ -2,16 +2,19 @@ const navIcon = document.getElementById("nav-icon");
 const navMenu = document.getElementsByTagName('nav')[0];
 const mainContent = document.getElementsByTagName('main')[0].children;
 const closeButton = document.getElementsByClassName('close')
+
+//animates the navigation icon
 navIcon.addEventListener('click',()=> {
   navIcon.classList.toggle("change");
   navMenu.classList.toggle("changeNav");
   navMenu.previousElementSibling.classList.toggle("changeSubheading");
-  
 
+  //toggles the class of 'show' for the main content divs
   for (let i=0; i < mainContent.length; i++) {
     mainContent[i].classList.toggle('show');
   }
 
+  //adds the class of 'slide in' when a nav item is clicked
   for (let i=0; i < navMenu.children.length; i++) {
     navMenu.children[i].addEventListener('click', ()=> {
       mainContent[i].classList.toggle('slideIn');
@@ -19,6 +22,7 @@ navIcon.addEventListener('click',()=> {
     });
   }
 
+  // slides the content out by toggling the class of 'slide in'
   for (let i=0; i < closeButton.length; i++) {
     closeButton[i].addEventListener('click', ()=> {
       mainContent[i].classList.toggle('slideIn');

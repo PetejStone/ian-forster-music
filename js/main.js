eb5e5212d82d8f1d18cb4c8a550c043655aeb044
PetejStone <petejstoney@gmail.com>
2018-11-17 13:08:34 -0800

    added files

--- js/main.js
+++ js/main.js
@@ -2,12 +2,13 @@ const navIcon = document.getElementById("nav-icon");
 const navMenu = document.getElementsByTagName('nav')[0];
 const mainContent = document.getElementsByTagName('main')[0].children;
 const closeButton = document.getElementsByClassName('close');
-const homeContent = document.querySelector('body');
+const homeContent = document.querySelector('.home-content');
+const footer = document.querySelector('footer');
 
 //animates the navigation icon
 navIcon.addEventListener('click',()=> {
   navIcon.classList.toggle("change");
-  navMenu.classList.toggle("changeNav");
+  navMenu.children[0].classList.toggle("changeNav");
   navMenu.previousElementSibling.classList.toggle("changeSubheading");
 
   //toggles the class of 'show' for the main content divs
@@ -16,19 +17,95 @@ navIcon.addEventListener('click',()=> {
   }
 
   //adds the class of 'slide in' when a nav item is clicked
-  for (let i=0; i < navMenu.children.length; i++) {
-    navMenu.children[i].addEventListener('click', ()=> {
+  for (let i=0; i < navMenu.children[0].children.length; i++) {
+    navMenu.children[0].children[i].addEventListener('touchmove', ()=> {
+
       mainContent[i].classList.toggle('slideIn');
-      homeContent.classList.toggle('stop-scrolling');
+
+
+      homeContent.style.position = 'fixed';
+      footer.style.display = 'none';
+
       console.log([i]);
     });
   }
 
+  for (let i=0; i < navMenu.children[0].children.length; i++) {
+    navMenu.children[0].children[i].addEventListener('click', ()=> {
+
+      mainContent[i].classList.toggle('slideIn');
+
+
+      homeContent.style.position = 'fixed';
+      footer.style.display = 'none';
+
+      console.log([i]);
+    });
+  }
+
+
+
+
+
+
+
+
+
   // slides the content out by toggling the class of 'slide in'
+  for (let i=0; i < closeButton.length; i++) {
+    closeButton[i].addEventListener('touchmove', ()=> {
+      mainContent[i].classList.toggle('slideIn');
+
+      homeContent.style.position = 'initial';
+      footer.style.display = 'initial';
+
+    });
+  }
+
   for (let i=0; i < closeButton.length; i++) {
     closeButton[i].addEventListener('click', ()=> {
       mainContent[i].classList.toggle('slideIn');
-      homeContent.classList.toggle('stop-scrolling');
+
+      homeContent.style.position = 'initial';
+      footer.style.display = 'initial';
+
     });
   }
+
 });
+
+
+
+//video
+
+
+  // 2. This code loads the IFrame Player API code asynchronously.
+  const tag = document.createElement('script');
+
+  tag.src = "https://www.youtube.com/iframe_api";
+  const firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // 3. This function creates an <iframe> (and YouTube player)
+  //    after the API code downloads.
+  var player;
+
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '400',
+      width: '600',
+      videoId: 'UjISkgBlJxc',
+      playerVars: {
+        controls: 1,
+        showinfo: 0,
+        color: 'white',
+
+      }
+    });
+
+
+  // 4. The API will call this function when the video player is ready.
+  function onPlayerReady(event) {
+    event.target.playVideo();
+  }
+}
